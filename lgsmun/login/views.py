82718a20eeb9ee659b970376891742049feb7c18
s-malik03 <safiy.malik@gmail.com>
2021-01-07 00:32:55 +0500

    added authentication

--- lgsmun/login/views.py
+++ lgsmun/login/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+from hashlib import sha256
 
 # Create your views here.
 
@@ -9,4 +11,18 @@ def index(request):
 
 def post_login(request):
 
-    return 0
+    if request.method=="POST":
+
+        info=request.POST
+
+        password=User.objects.get(email=request.POST["email"])
+
+        password=password.password
+
+        if password==sha256(request.POST["password"].encode('utf-8')).hexdigest():
+
+            return HttpResponse('nice')
+
+        else:
+
+            return HttpResponse('not')
