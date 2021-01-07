e100fe92bc6719fe67cdec6ea939ae65d248cf06
s-malik03 <safiy.malik@gmail.com>
2021-01-07 21:57:18 +0500

    set up for ali zaidi

--- lgsmun/dashboards/views.py
+++ lgsmun/dashboards/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+
+    return HttpResponse("hi")
+
+def timer(request):
+
+    request_context={}
+
+    return render(request,'timer.html',request_context)
 
 # Create your views here.
--- lgsmun/lgsmun/urls.py
+++ lgsmun/lgsmun/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',include("login.urls")),
-    path('menu/',include('menu.urls'))
+    path('menu/',include('menu.urls')),
+    path('dashboard/',include('dashboard.urls'))
 ]
