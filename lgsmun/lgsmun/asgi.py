a18f8bfbb3413484d741a969b3049c7c4655a3dc
s-malik03 <safiy.malik@gmail.com>
2021-01-15 23:44:10 +0500

    added django.setup

--- lgsmun/lgsmun/asgi.py
+++ lgsmun/lgsmun/asgi.py
@@ -15,6 +15,8 @@ from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgsmun.settings')
 
+django.setup()
+
 application = get_default_application()
 
 #
