3d7a5540cbda72be1d241a8f92576346725dd359
s-malik03 <safiy.malik@gmail.com>
2021-01-15 23:29:50 +0500

    added redis

--- lgsmun/lgsmun/asgi.py
+++ lgsmun/lgsmun/asgi.py
@@ -9,21 +9,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+import django
 
-from channels.routing import ProtocolTypeRouter,URLRouter
-
-from channels.auth import AuthMiddlewareStack
-
-from dashboards.routing import ws_urlpatterns
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lgsmun.settings')
 
-application = ProtocolTypeRouter({
-
-    'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(URLRouter(ws_urlpatterns))
-
-})
+application = get_default_application()
 
 #
